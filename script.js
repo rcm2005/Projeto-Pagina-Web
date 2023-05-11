f12970ff542265df581a70fc26907180e5d1fba3
rafael <rcm2005@icloud.com>
2023-05-11 16:23:16 -0300

    adicionando melhorias para a pagina, tais como as caixas de input na pagina da calculadora, e modificando o css

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function validar(){
     alert('preencha todos os campos')
     }else{
     alert('acesso permitido')
-    window.open("menu.html")
+    window.open("menu.html");
     }
 }
 
@@ -15,25 +15,42 @@ function validar(){
 
 function calc(){
 
-    let n1 = number(prompt('Digite o primeiro valor'))
-    let n2 = number(prompt('Digite o segundo valor'))
-    let op = number(prompt(`Dados informados ${n1} e ${n2} ?n Escolha uma opção /n 1 - Somar/n 2 - Subtrair /n 3 - multiplicar /n 4 - dividir `))
+    // let n1 = Number(prompt('Digite o primeiro valor:'))
+    // let n2 = Number(prompt('Digite o segundo valor:'))
+   
+    let n1 = document.getElementById('entry1').value
+    // let n1 = parseFloat(n1s)
 
-   let msg = document.getElementById('msg')
-   msg.innerHTML = `<h1> Processando o resultado</h1>`
+    let n2 = document.getElementById('entry2').value
+    // let n2 = parseFloat(n2s)
+    // let op = Number(prompt(`Dados informados ${n1} e ${n2} ?n Escolha uma opção \n1- Somar \n2- Subtrair \n3- multiplicar \n4- dividir `))
 
-   switch(op){
+    let ops = document.getElementById('op').value
+    let op = parseFloat(ops)
+
+    let msg = document.getElementById('msg')
+    msg.innerHTML = `<h2> Processando o resultado...</h2>`
+
+    // if (entry1 == "" && entry2 == ""){
+    //     prompt('Por favor, preencha todos os campos')
+    // }
+
+   switch(op){ //a estrutura swich é parecida com a match case do py, e funciona com numeros inteiros e strings
         case 1:
+            // template de strings é feito com `` e utiliza o {} para interpolar as strings 
             msg.innerHTML += `<p>${n1} + ${n2} = <strong>${n1 + n2}</strong></p>`
-            break
+            break // break é obrigatório em cada case, para não executar comandos de outros cases
         case 2:
              msg.innerHTML += `<p>${n1} - ${n2} = <strong>${n1 - n2}</strong></p>`
              break
-        case 2:
-             msg.innerHTML += `<p>${n1} * ${n2} = <strong>${n1 * n2}</strong></p>`
-             break
         case 3:
+             msg.innerHTML += `<p>${n1} x ${n2} = <strong>${n1 * n2}</strong></p>`
+             break
+        case 4:
             msg.innerHTML += `<p>${n1} / ${n2} = <strong>${n1 / n2}</strong></p>`
             break
+        default: 
+        msg.innerHTML += `<p>OPÇÃO INVÁLIDA ! Você digitou ${n1} e ${n2}</p> `
+        break
    }
 }
